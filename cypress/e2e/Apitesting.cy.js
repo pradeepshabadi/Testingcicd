2557556b2fed001ed1a22e694d488dbaa68a46bf
Pradeepkumar Shabadi <pradeepkumar.shabadi@valtech.com>
2025-02-03 14:03:21 +0530

    changes

--- cypress/e2e/Apitesting.cy.js
+++ cypress/e2e/Apitesting.cy.js
@@ -1,39 +1,39 @@
 
-// describe('api testing', ()=>{
+describe('api testing', ()=>{
 
 
-//   it('mock response',()=>{
+  it('mock response',()=>{
 
-//     //cy.intecept(reuestobject, responseobject) takes 2 parameters as request and response object
-//     //cy.intercept({requestobject},{responseobject})
-//     cy.visit('https://rahulshettyacademy.com/angularAppdemo/');
+    //cy.intecept(reuestobject, responseobject) takes 2 parameters as request and response object
+    //cy.intercept({requestobject},{responseobject})
+    cy.visit('https://rahulshettyacademy.com/angularAppdemo/');
 
 
-//     cy.intercept({
+    cy.intercept({
 
-//       method: 'GET',
-//       url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
-//     }, 
+      method: 'GET',
+      url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+    }, 
     
-//     {
-//       statusCode: 200,
-//       body: [{
-//             "book_name": "RestAssured with Java",
-//             "isbn": "LSA",
-//             "aisle": "2303"
-//         }]
+    {
+      statusCode: 200,
+      body: [{
+            "book_name": "RestAssured with Java",
+            "isbn": "LSA",
+            "aisle": "2303"
+        }]
       
 
 
-//   }).as('bookreply')
-//     cy.get(' .btn-primary').click();
-//     cy.wait('@bookreply');
-//     cy.get('p').should('have.text','Oops only 1 Book available');
+  }).as('bookreply')
+    cy.get(' .btn-primary').click();
+    cy.wait('@bookreply');
+    cy.get('p').should('have.text','Oops only 1 Book available');
 
     
 
 
-//   })
+  })
 
 //   it('request testing',()=>{
 
@@ -58,5 +58,5 @@
 
 //   })
 
-// })
+ })
 
